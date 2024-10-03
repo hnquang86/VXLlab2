06955dec3bf894ec959ecb2ecd2c89df22f3cab3
LAPTOP-LBNRUB29\Admin <trickerhnq2004@gmail.com>
2024-10-03 11:40:19 +0700

    EX3

--- Projectlab2/Core/Inc/7segment.h
+++ Projectlab2/Core/Inc/7segment.h
@@ -10,8 +10,14 @@
 #include "main.h"
 
 extern const uint8_t digits[10][7];
+extern const int MAX_LED;
+extern int index_led;
+extern int led_buffer[4];
 
 
+void update7SEG(int index);
+
+void setDigit(int digit);
 void display7SEG(int num);
 
 
--- Projectlab2/Core/Src/7segment.c
+++ Projectlab2/Core/Src/7segment.c
@@ -21,6 +21,10 @@
     {0, 0, 0, 0, 1, 0, 0}   // 9
 };
 
+ const int MAX_LED = 4;
+ int index_led = 0;
+ int led_buffer[4] = {1, 2, 3, 4};
+
 void setDigit(int digit){
     HAL_GPIO_WritePin(SEG0_GPIO_Port, SEG0_Pin, digits[digit][0] ? GPIO_PIN_SET : GPIO_PIN_RESET);
     HAL_GPIO_WritePin(SEG1_GPIO_Port, SEG1_Pin, digits[digit][1] ? GPIO_PIN_SET : GPIO_PIN_RESET);
@@ -34,4 +38,42 @@ void setDigit(int digit){
 void display7SEG(int num){
 	setDigit(num);
 }
+
+
+void update7SEG(int index) {
+    switch (index) {
+        case 0:
+            HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, GPIO_PIN_SET);
+            display7SEG(led_buffer[0]);
+            break;
+        case 1:
+            HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, GPIO_PIN_SET);
+            display7SEG(led_buffer[1]);
+            break;
+        case 2:
+            HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, GPIO_PIN_SET);
+            display7SEG(led_buffer[2]);
+            break;
+        case 3:
+            HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, GPIO_PIN_RESET);
+            display7SEG(led_buffer[3]);
+            break;
+        default:
+            break;
+    }
+}
+
+
 
